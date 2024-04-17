945bc56983eff8a96901940f02d0d66cb81f86be
looxar <looxar.wicher@gmail.com>
2024-04-17 23:43:56 +0700

    get post OK

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,37 @@
-import { Request, Response, NextFunction } from 'express';
-const cors = require('cors')
-const express = require('express')
-const { PrismaClient } = require('@prisma/client')
+import bodyParser from "body-parser";
+import { Request, Response, NextFunction } from "express";
+const cors = require("cors");
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient()
-const app = express()
+const prisma = new PrismaClient();
+const app = express();
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors({
-  origin: ['http://localhost:8001','http://localhost:3000','http://localhost:8081'],
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:8001",
+      "http://localhost:3000",
+      "http://localhost:8081",
+    ],
+  })
+);
 
 app.post(`/signup`, async (req: Request, res: Response) => {
   try {
-    const { user_name, email, password } = req.body;
-
+    const {
+      params: { user_name, email, password },
+    } = req.body;
     // Generate salt and hash the password
     const saltRounds = 10; // Number of salt rounds (recommended value)
     const salt = bcrypt.genSaltSync(saltRounds);
@@ -28,7 +42,7 @@ app.post(`/signup`, async (req: Request, res: Response) => {
         user_name: user_name,
         email: email,
         password: hashedPassword, // Store the hashed password in the database
-        salt: salt // Store the salt in the database
+        salt: salt, // Store the salt in the database
       },
     });
 
@@ -39,19 +53,19 @@ app.post(`/signup`, async (req: Request, res: Response) => {
   }
 });
 
-
 app.post(`/login`, async (req: Request, res: Response) => {
   try {
-    const { email, password } = req.body;
-
+    const {
+      params: { email, password },
+    } = req.body;
     // Retrieve user from the database based on the provided email
     const user = await prisma.user.findUnique({
-      where: { email: email }
+      where: { email: email },
     });
 
     // If the user doesn't exist or the provided email is incorrect, return an error
     if (!user) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: "Invalid email or password" });
     }
 
     // Compare the provided password with the hashed password stored in the database
@@ -59,74 +73,90 @@ app.post(`/login`, async (req: Request, res: Response) => {
 
     // If the passwords don't match, return an error
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: "Invalid email or password" });
     }
 
     // If authentication is successful, return a success message or user data
-    res.status(200).json({ message: 'Login successful', user: user });
+    res.status(200).json({ message: "Login successful", user: user });
   } catch (error) {
     console.error("Error logging in:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
-  const checkMemberRole = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      // Your middleware logic here
-      next();
-    } catch (error) {
-      console.error('Error in checkMemberRole middleware:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  };
+const checkMemberRole = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    // Your middleware logic here
+    next();
+  } catch (error) {
+    console.error("Error in checkMemberRole middleware:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
-  app.get("/user/all", async (req: Request, res: Response) => {
-    try {
-      const users = await prisma.user.findMany({
-        select: {
-          id: true,
-          email: true,
-          role: true,
-          user_name: true
-        }
-      });
-  
-      res.json(users);
-    } catch (error) {
-      console.error("Error retrieving users:", error);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  });
+app.get("/user/all", async (req: Request, res: Response) => {
+  try {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        role: true,
+        user_name: true,
+      },
+    });
 
-  app.get("/recipe/all", async (req: Request, res: Response) => {
-    try {
-      const recipes = await prisma.recipe.findMany({
-        include: {
-          duration: true,
-          difficult: true,
-          user: {
-            select: {
-              id: true,
-              email: true,
-              user_name: true
-            }
+    res.json(users);
+  } catch (error) {
+    console.error("Error retrieving users:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/recipe/all", async (req: Request, res: Response) => {
+  try {
+    const recipes = await prisma.recipe.findMany({
+      include: {
+        duration: true,
+        difficult: true,
+        user: {
+          select: {
+            id: true,
+            email: true,
+            user_name: true,
           },
-        }
-      });
-  
-      res.json(recipes);
-    } catch (error) {
-      console.error("Error retrieving recipes:", error);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  });
+        },
+      },
+    });
 
-  app.post(`/recipecreate`, 
+    res.json(recipes);
+  } catch (error) {
+    console.error("Error retrieving recipes:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post(
+  `/recipecreate`,
   // checkMemberRole ,
-   async (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
+    console.log("req.body : " + JSON.stringify(req.body));
     try {
-      const { menu_name, pathimg, raw_material, step, duration, difficult, user } =
-        req.body;
+      const {
+        params: {
+          menu_name,
+          pathimg,
+          raw_material,
+          step,
+          duration,
+          difficult,
+          user,
+        },
+      } = req.body;
+
       const result = await prisma.recipe.create({
         data: {
           menu_name: menu_name,
@@ -149,20 +179,25 @@ app.post(`/login`, async (req: Request, res: Response) => {
       console.error("Error create recipe:", error);
       res.status(500).json({ error: "Internal server error" });
     }
-  });
+  }
+);
 
 app.post("/recipe/byduration", async (req: Request, res: Response) => {
   try {
-    const { setDuration } = req.body;
-
+    console.log("req.body : " + JSON.stringify(req.body));
+    const {
+      params: { setDuration },
+    } = req.body;
     // Check if setDuration is provided and is a valid number
     if (!setDuration || isNaN(parseInt(setDuration))) {
-      return res.status(400).json({ error: "Invalid or missing 'setDuration' parameter" });
+      return res
+        .status(400)
+        .json({ error: "Invalid or missing 'setDuration' parameter" });
     }
 
     const recipes = await prisma.recipe.findMany({
       where: {
-        durationId: parseInt(setDuration)
+        durationId: parseInt(setDuration),
       },
       include: {
         duration: true,
@@ -171,12 +206,11 @@ app.post("/recipe/byduration", async (req: Request, res: Response) => {
           select: {
             id: true,
             email: true,
-            user_name: true
-          }
+            user_name: true,
+          },
         },
-      }
+      },
     });
-
     res.json(recipes);
   } catch (error) {
     console.error("Error retrieving recipes:", error);
@@ -186,16 +220,20 @@ app.post("/recipe/byduration", async (req: Request, res: Response) => {
 
 app.post("/recipe/bylevel", async (req: Request, res: Response) => {
   try {
-    const { setLevel } = req.body;
+    const {
+      params: { setLevel },
+    } = req.body;
 
     // Check if setDuration is provided and is a valid number
     if (!setLevel || isNaN(parseInt(setLevel))) {
-      return res.status(400).json({ error: "Invalid or missing 'setDuration' parameter" });
+      return res
+        .status(400)
+        .json({ error: "Invalid or missing 'setDuration' parameter" });
     }
 
     const recipes = await prisma.recipe.findMany({
       where: {
-        levelId: parseInt(setLevel)
+        levelId: parseInt(setLevel),
       },
       include: {
         difficult: true,
@@ -204,10 +242,52 @@ app.post("/recipe/bylevel", async (req: Request, res: Response) => {
           select: {
             id: true,
             email: true,
-            user_name: true
-          }
+            user_name: true,
+          },
         },
-      }
+      },
+    });
+
+    res.json(recipes);
+  } catch (error) {
+    console.error("Error retrieving recipes:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/recipe/bybothcon", async (req: Request, res: Response) => {
+  try {
+    const {
+      params: { setLevel, setDuration },
+    } = req.body;
+    // Check if setDuration is provided and is a valid number
+    if (!setLevel || isNaN(parseInt(setLevel))) {
+      return res
+        .status(400)
+        .json({ error: "Invalid or missing 'setDuration' parameter" });
+    }
+    if (!setDuration || isNaN(parseInt(setDuration))) {
+      return res
+        .status(400)
+        .json({ error: "Invalid or missing 'setDuration' parameter" });
+    }
+
+    const recipes = await prisma.recipe.findMany({
+      where: {
+        levelId: parseInt(setLevel),
+        durationId: parseInt(setDuration),
+      },
+      include: {
+        difficult: true,
+        duration: true,
+        user: {
+          select: {
+            id: true,
+            email: true,
+            user_name: true,
+          },
+        },
+      },
     });
 
     res.json(recipes);
@@ -220,6 +300,5 @@ app.post("/recipe/bylevel", async (req: Request, res: Response) => {
 const server = app.listen(8080, () =>
   console.log(`
 🚀 Server ready at: http://localhost:8080
-⭐️ See sample requests: http://pris.ly/e/js/rest-express#3-using-the-rest-api`),
-)
-
+⭐️ See sample requests: http://pris.ly/e/js/rest-express#3-using-the-rest-api`)
+);
